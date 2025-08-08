2793447abc354722cafa05625dad7a36f82fb7af
Matias-Rosales <marosales2009@gmail.com>
2025-08-08 11:37:48 -0300

    Js 0

--- Front-end/front-end.js
+++ Front-end/front-end.js
@@ -1,10 +1,1 @@
-let clickrules = document.getElementById("reglamento");
-function test1 () {
-alert("funciona");
-};
-clickrules.addEventListener("click", test1);
-let clickplay = document.getElementById("jugar");
-function test2 () {
-alert("JUGAR");
-};
-clickplay.addEventListener("click", test2);
+

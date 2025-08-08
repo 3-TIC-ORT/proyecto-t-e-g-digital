b1153ec8f2dcbb8cf09d34f151fd39b9df28bebb
Your Name <you@example.com>
2025-08-08 15:51:00 -0300

    8/8 2

--- Front-end/front-end.js
+++ Front-end/front-end.js
@@ -1,1 +1,21 @@
-
+let players = document.getElementById("playersid");
+function test3 () {
+alert(parseInt(players.value));
+};
+let objetivos = document.getElementById("objetivosid");
+function test4 () {
+if(parseInt(objetivos.value)===0){
+msg="ERROR";
+alert(msg);
+};
+if(parseInt(objetivos.value)===1){
+msg="Sin";
+alert(msg);
+};
+if(parseInt(objetivos.value)===2){
+msg="Con";
+alert(msg);
+};
+}
+let ready = document.getElementById("gameready");
+ready.addEventListener("click", test4)

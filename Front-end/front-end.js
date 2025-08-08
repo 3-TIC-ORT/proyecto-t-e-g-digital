c47daaf3b07a30c8ff515d35accdd16b6867ed77
Matias-Rosales <marosales2009@gmail.com>
2025-08-08 11:19:52 -0300

    Test 2

--- Front-end/front-end.js
+++ Front-end/front-end.js
@@ -1,5 +1,10 @@
-let clickrules = document.getElementById("reglamento")
+let clickrules = document.getElementById("reglamento");
 function test1 () {
-alert("funciona")
-}
-clickrules.addEventListener("click", test1)
+alert("funciona");
+};
+clickrules.addEventListener("click", test1);
+let clickplay = document.getElementById("jugar");
+function test2 () {
+alert("JUGAR");
+};
+clickplay.addEventListener("click", test2);

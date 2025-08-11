12a65c22297779a6ea26f1694f2bf64580ba8f74
Matias-Rosales <marosales2009@gmail.com>
2025-08-11 15:15:15 -0300

    js 11/8

--- Front-end/front-end.js
+++ Front-end/front-end.js
@@ -1,21 +1,22 @@
 let players = document.getElementById("playersid");
 function test3 () {
-alert(parseInt(players.value));
+alert(parseInt(players.value) + " jugadores");
 };
 let objetivos = document.getElementById("objetivosid");
 function test4 () {
 if(parseInt(objetivos.value)===0){
-msg="ERROR";
+msg="SELECCIONAR OBJETIVOS SECRETOS";
 alert(msg);
 };
 if(parseInt(objetivos.value)===1){
-msg="Sin";
+msg="Sin Objetivos Secretos";
 alert(msg);
 };
 if(parseInt(objetivos.value)===2){
-msg="Con";
+msg="Con Objetivos Secretos";
 alert(msg);
 };
 }
 let ready = document.getElementById("gameready");
-ready.addEventListener("click", test4)
+ready.addEventListener("click", test3);
+ready.addEventListener("click", test4);

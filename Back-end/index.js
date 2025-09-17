d6dd80bd9c053ca2e64650c8d47dd1be3a4135c7
Dantedebadts <dantepetrusdebadts@gmail.com>
2025-09-17 19:28:09 -0300

    17/9

--- Back-end/index.js
+++ Back-end/index.js
@@ -2,11 +2,9 @@ import fs from "fs";
 import { subscribePOSTEvent, startServer } from "soquetic";
 
 function guardarEstado(estado) {
-    let data = JSON.parse(fs.readFileSync("./datos/datos.json", "utf-8"));
-    data.push(estado);
-    fs.writeFileSync("./datos/datos.json", JSON.stringify(data, null, 2));
+   fs.writeFileSync("./datos/datos.json", JSON.stringify(estado, null, 2));
 }
 
 subscribePOSTEvent("guardarEstado", (estado) => guardarEstado(estado));
 
-startServer(3000, true);
+startServer(3015, true);

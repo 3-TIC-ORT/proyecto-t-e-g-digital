687f46743c0dcef13118a3b36b4a9cec5991199c
dantedebadts <49836521@est.ort.edu.ar>
2025-09-26 08:39:17 -0300

    instructor prep

--- Front-end/PREPARACION/prep.js
+++ Front-end/PREPARACION/prep.js
@@ -1,29 +1,40 @@
 let players = document.getElementById("playersid");
 let objetivos = document.getElementById("objetivosid");
+let instructor = document.getElementById("instructorid")
 let advertencia = document.getElementById("advertencia");
 let contenidojug = document.getElementById("contenidojug");
 let contenidoobj = document.getElementById("contenidoobj");
 let cerrar = document.getElementById("cerrar");
+let contenidoins = document.getElementById("contenidoins");
 function test3 () {
 if (parseInt(players.value) === 0){
 contenidojug.textContent = "SELECCIONE CANTIDAD DE JUGADORES";
 } else {
-contenidojug.textContent = parseInt(players.value) + " jugadores";
+contenidojug.textContent = parseInt(players.value) + " JUGADORES";
 };
 if(parseInt(objetivos.value)===0){
 contenidoobj.textContent = "SELECCIONAR OBJETIVOS SECRETOS";  
 };
 if(parseInt(objetivos.value)===1){
-contenidoobj.textContent = "Sin Objetivos Secretos";
+contenidoobj.textContent = "SIN OBJETIVOS SECRETOS";
 };
 if(parseInt(objetivos.value)===2){
-contenidoobj.textContent ="Con Objetivos Secretos";
+contenidoobj.textContent ="CON OBJETIVOS SECRETOS";
+};
+if(parseInt(instructor.value) === 0){
+    contenidoins.textContent = "SELECCIONAR USO DE INSTRUCTOR";
+}
+if(parseInt(instructor.value) === 1){
+    contenidoins.textContent = "CON INSTRUCTOR";
+}
+if(parseInt(instructor.value) === 2){
+    contenidoins.textContent = "SIN INSTRUCTOR";
 };
 advertencia.showModal();
-if(parseInt(players.value) != 0 && parseInt(objetivos.value) != 0) {
+if(parseInt(players.value) != 0 && parseInt(objetivos.value) != 0 && parseInt(instructor.value)!= 0) {
 window.location.href = "../JUEGO/game.html"
 };
-};
+}
 let ready = document.getElementById("gameready");
 ready.addEventListener("click", test3);
 cerrar.addEventListener("click", ()=> advertencia.close());
